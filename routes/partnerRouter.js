b87e00ac42dca24aeef02b91a05ca340316fea8a
Steven Olmos <eaodesigns@gmail.com>
2022-11-07 19:49:32 -0800

    Express Generator + NodeMon

---
+++ routes/partnerRouter.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const partnerRouter = express.Router();
+
+partnerRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end("Will send all the partner to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the partner: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /partner");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all partners");
+  });
+
+partnerRouter
+  .route("/:partnerId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(`Will send details of partners: ${req.params.partnerId} to you`);
+  })
+  .post((req, res) => {
+    res.end(
+      `POST operation not supported on /campsites/${req.params.partnerId}`
+    );
+  })
+  .put((req, res) => {
+    res.write(`Updating the partners: ${req.params.partnerId}\n`);
+    res.end(
+      `Will update the partners: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .delete((req, res) => {
+    res.end("Deleting all partners");
+  });
+
+module.exports = partnerRouter;
